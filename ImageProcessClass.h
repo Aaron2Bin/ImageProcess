7f4cc49c880adae9154789bec190d827f4c32e8b
Aaron2Bin <49354436+Aaron2Bin@users.noreply.github.com>
2019-08-24 21:15:11 +0800

    Update ImageProcessClass.h

--- ImageProcessClass.h
+++ ImageProcessClass.h
@@ -1,8 +1,11 @@
 #include <opencv2/opencv.hpp>
+#include <string>
+using namespace std;
 using namespace cv;
 class ImageProcess
 {
 public:
+ImageProcess(string str);
 void HisEqual(Mat &src, Mat &dst);
 private:
 Mat src_img;
